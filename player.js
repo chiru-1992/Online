8b4f274b9e8246a51dc332070e1fc7dbf0cef5d1
chiru-1992 <chirugurung730@gmail.com>
2026-06-30 22:50:04 +0530

    Update and rename script.js to player.js

---
+++ player.js
@@ -0,0 +1,141 @@
+export const players = [
+
+{
+ticket:1,
+portugal:{name:"Cristiano Ronaldo",jersey:7},
+croatia:{name:"Luka Modrić",jersey:10}
+},
+
+{
+ticket:2,
+portugal:{name:"Bruno Fernandes",jersey:8},
+croatia:{name:"Joško Gvardiol",jersey:4}
+},
+
+{
+ticket:3,
+portugal:{name:"Bernardo Silva",jersey:10},
+croatia:{name:"Mateo Kovačić",jersey:8}
+},
+
+{
+ticket:4,
+portugal:{name:"Rafael Leão",jersey:17},
+croatia:{name:"Ivan Perišić",jersey:14}
+},
+
+{
+ticket:5,
+portugal:{name:"Diogo Jota",jersey:21},
+croatia:{name:"Andrej Kramarić",jersey:9}
+},
+
+{
+ticket:6,
+portugal:{name:"João Félix",jersey:11},
+croatia:{name:"Lovro Majer",jersey:7}
+},
+
+{
+ticket:7,
+portugal:{name:"Vitinha",jersey:23},
+croatia:{name:"Mario Pašalić",jersey:15}
+},
+
+{
+ticket:8,
+portugal:{name:"João Neves",jersey:15},
+croatia:{name:"Petar Sučić",jersey:21}
+},
+
+{
+ticket:9,
+portugal:{name:"Rúben Dias",jersey:3},
+croatia:{name:"Josip Šutalo",jersey:6}
+},
+
+{
+ticket:10,
+portugal:{name:"Nuno Mendes",jersey:19},
+croatia:{name:"Josip Stanišić",jersey:2}
+},
+
+{
+ticket:11,
+portugal:{name:"João Cancelo",jersey:20},
+croatia:{name:"Borna Sosa",jersey:3}
+},
+
+{
+ticket:12,
+portugal:{name:"Diogo Dalot",jersey:5},
+croatia:{name:"Josip Juranović",jersey:22}
+},
+
+{
+ticket:13,
+portugal:{name:"Gonçalo Ramos",jersey:9},
+croatia:{name:"Bruno Petković",jersey:17}
+},
+
+{
+ticket:14,
+portugal:{name:"Pedro Neto",jersey:7},
+croatia:{name:"Nikola Vlašić",jersey:13}
+},
+
+{
+ticket:15,
+portugal:{name:"Francisco Conceição",jersey:26},
+croatia:{name:"Martin Baturina",jersey:10}
+},
+
+{
+ticket:16,
+portugal:{name:"Palhinha",jersey:6},
+croatia:{name:"Kristijan Jakić",jersey:14}
+},
+
+{
+ticket:17,
+portugal:{name:"António Silva",jersey:4},
+croatia:{name:"Marin Pongračić",jersey:5}
+},
+
+{
+ticket:18,
+portugal:{name:"Gonçalo Inácio",jersey:14},
+croatia:{name:"Duje Ćaleta-Car",jersey:19}
+},
+
+{
+ticket:19,
+portugal:{name:"Renato Veiga",jersey:13},
+croatia:{name:"Luka Sučić",jersey:11}
+},
+
+{
+ticket:20,
+portugal:{name:"Rúben Neves",jersey:18},
+croatia:{name:"Igor Matanović",jersey:20}
+},
+
+{
+ticket:21,
+portugal:{name:"Nelson Semedo",jersey:2},
+croatia:{name:"Marco Pašalić",jersey:16}
+},
+
+{
+ticket:22,
+portugal:{name:"João Mário",jersey:12},
+croatia:{name:"Franjo Ivanović",jersey:18}
+},
+
+{
+ticket:23,
+portugal:{name:"Geovany Quenda",jersey:27},
+croatia:{name:"Toni Fruk",jersey:23}
+}
+
+];
